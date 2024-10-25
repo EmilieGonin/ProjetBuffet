6950af325dd1a05c7079f621f84910ee51ba3cee
Emilie Gonin <emilie.yuu@gmail.com>
2024-10-25 19:11:32 +0200

    working on catalog scene

---
+++ Assets/Scripts/SO/RecipeSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RecipeSO", menuName = "Scriptable Objects/RecipeSO")]
+public class RecipeSO : ScriptableObject
+{
+    public string Name;
+    public int Price;
+    // Asset 3D plat
+    // Asset 3D portion
+}
