551d6cbb148119b07094e1cd55c7c1514bcf0ea4
Emilie Gonin <emilie.yuu@gmail.com>
2024-10-26 18:32:24 +0200

    improve catalog + add main menu scene

--- Assets/Scripts/SO/FurnitureSO.cs
+++ Assets/Scripts/SO/FurnitureSO.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "FurnitureSO", menuName = "Scriptable Objects/FurnitureSO")]
-public class FurnitureSO : ScriptableObject
+public class FurnitureSO : BuyableElementSO
 {
-    public string Name;
-    public int Price;
-    public GameObject Prefab;
+    //
 }
