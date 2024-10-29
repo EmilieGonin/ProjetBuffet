a7568ef26ae6196364eac5398a2d9b0236988163
Emilie Gonin <emilie.yuu@gmail.com>
2024-10-29 17:15:34 +0100

    add customer spawner logic

--- Assets/Scripts/CustomersSpawner.cs
+++ Assets/Scripts/CustomersSpawner.cs
@@ -1,10 +1,21 @@
 using NaughtyAttributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomersSpawner : MonoBehaviour
 {
+    [Header("Dependencies")]
+    [HorizontalLine(color: EColor.Red)]
     [SerializeField] private List<GameObject> _customersPrefabs;
+
+    [Header("Settings")]
+    [HorizontalLine(color: EColor.Blue)]
+    [SerializeField] private int _minTime = 5;
+    [SerializeField] private int _maxTime = 10;
+    [SerializeField] private int _maxCustomers = 10;
+
+    private int _customersCount;
 
     private void Awake()
     {
@@ -14,8 +25,21 @@ public class CustomersSpawner : MonoBehaviour
     [Button(enabledMode: EButtonEnableMode.Playmode)]
     private void Spawn()
     {
-        int random = Random.Range(0, _customersPrefabs.Count);
+        int random = UnityEngine.Random.Range(0, _customersPrefabs.Count);
 
         Instantiate(_customersPrefabs[random], transform.position, Quaternion.identity);
+    }
+
+    [Button(enabledMode: EButtonEnableMode.Playmode)]
+    private IEnumerator StartSpawn()
+    {
+        while (_customersCount < _maxCustomers)
+        {
+            Spawn();
+            int random = UnityEngine.Random.Range(_minTime, _maxTime);
+            yield return new WaitForSeconds(random);
+        }
+
+        yield return null;
     }
 }
